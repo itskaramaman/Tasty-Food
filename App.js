2cefbbb469aae86bf2fca265c3703c8c7f3f52ef
Karamjeet Singh <singhkaramjeetapple@gmail.com>
2023-11-06 16:01:39 -0800

    started the food app

--- App.js
+++ App.js
@@ -1,21 +1,409 @@
-const heading = React.createElement(
-    'div',
-    {id:'parent'},
-    React.createElement(
-        'div',
-        {id:'child'},
-        [React.createElement(
-            'h1',
-            {},
-            'Namaste Inception h1'
-        ),
-        React.createElement(
-            'h2',
-            {},
-            'Namaste Inception h2'
-        )]
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-    )
-);
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(heading);
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img
+          className="logo"
+          src="https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png"
+        />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+const RestaurantCard = (props) => {
+  console.log(props.resData.info);
+  const {
+    name,
+    cuisines,
+    costForTwo,
+    avgRatingString,
+    cloudinaryImageId,
+    sla,
+  } = props.resData?.info;
+  return (
+    <div className="res-card">
+      <img
+        alt="card-log"
+        className="res-img"
+        src={
+          "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/" +
+          cloudinaryImageId
+        }
+      />
+      <h3>{name}</h3>
+      <h4>{cuisines?.join(", ")}</h4>
+      <small>Cost for Two: {costForTwo}</small>
+      <br />
+      <small>{avgRatingString} stars</small>
+      <br />
+      <small>{sla?.deliveryTime} min</small>
+    </div>
+  );
+};
+
+const resList = [
+  {
+    info: {
+      id: "70750",
+      name: "Surti Khaman House",
+      cloudinaryImageId: "vhqptzx28p1jo8dgxp1j",
+      locality: "Anand Mahal Road",
+      areaName: "Anand Mahal",
+      costForTwo: "₹180 for two",
+      cuisines: ["Desserts"],
+      avgRating: 3.9,
+      veg: true,
+      feeDetails: {
+        restaurantId: "70750",
+        fees: [
+          {
+            name: "BASE_DISTANCE",
+            fee: 4300,
+          },
+          {
+            name: "BASE_TIME",
+          },
+          {
+            name: "ANCILLARY_SURGE_FEE",
+          },
+        ],
+        totalFee: 4300,
+      },
+      parentId: "197899",
+      avgRatingString: "3.9",
+      totalRatingsString: "10K+",
+      sla: {
+        deliveryTime: 25,
+        lastMileTravel: 5.8,
+        serviceability: "SERVICEABLE",
+        slaString: "25 mins",
+        lastMileTravelString: "5.8 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2023-11-07 19:00:00",
+        opened: true,
+      },
+      badges: {
+        imageBadges: [
+          {
+            imageId: "v1695133679/badges/Pure_Veg111.png",
+            description: "pureveg",
+          },
+        ],
+      },
+      isOpen: true,
+      aggregatedDiscountInfoV2: {},
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {
+            badgeObject: [
+              {
+                attributes: {
+                  description: "pureveg",
+                  imageId: "v1695133679/badges/Pure_Veg111.png",
+                },
+              },
+            ],
+          },
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          mediaType: "ADS_MEDIA_ENUM_IMAGE",
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+    },
+    analytics: {},
+    cta: {
+      link: "https://www.swiggy.com/restaurants/surti-khaman-house-road-anand-mahal-surat-70750",
+      type: "WEBLINK",
+    },
+  },
+  {
+    info: {
+      id: "395938",
+      name: "Baskin Robbins - Ice Cream Desserts",
+      cloudinaryImageId: "85ccae4e3576f9330af102c46ca85395",
+      locality: "Char Rasta",
+      areaName: "Vesu",
+      costForTwo: "₹250 for two",
+      cuisines: ["Desserts", "Ice Cream"],
+      avgRating: 3.9,
+      veg: true,
+      feeDetails: {
+        restaurantId: "395938",
+        fees: [
+          {
+            name: "BASE_DISTANCE",
+            fee: 5300,
+          },
+          {
+            name: "BASE_TIME",
+          },
+          {
+            name: "ANCILLARY_SURGE_FEE",
+          },
+        ],
+        totalFee: 5300,
+      },
+      parentId: "5588",
+      avgRatingString: "3.9",
+      totalRatingsString: "500+",
+      sla: {
+        deliveryTime: 28,
+        lastMileTravel: 8,
+        serviceability: "SERVICEABLE",
+        slaString: "28 mins",
+        lastMileTravelString: "8.0 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2023-11-08 05:00:00",
+        opened: true,
+      },
+      badges: {},
+      isOpen: true,
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {},
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      aggregatedDiscountInfoV3: {
+        header: "10% OFF",
+        subHeader: "ABOVE ₹1000",
+        discountTag: "FLAT DEAL",
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          mediaType: "ADS_MEDIA_ENUM_IMAGE",
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+    },
+    analytics: {},
+    cta: {
+      link: "https://www.swiggy.com/restaurants/baskin-robbins-ice-cream-desserts-char-rasta-vesu-surat-395938",
+      type: "WEBLINK",
+    },
+  },
+  {
+    info: {
+      id: "563098",
+      name: "\tURBAN EGG",
+      cloudinaryImageId: "xqxmcstn754z6elymsuu",
+      locality: "Ghodod Road",
+      areaName: "Athwa",
+      costForTwo: "₹300 for two",
+      cuisines: ["Chinese", "Ice Cream", "Pizzas", "Biryani"],
+      avgRating: 4.3,
+      feeDetails: {
+        restaurantId: "563098",
+        fees: [
+          {
+            name: "BASE_DISTANCE",
+            fee: 2300,
+          },
+          {
+            name: "BASE_TIME",
+          },
+          {
+            name: "ANCILLARY_SURGE_FEE",
+          },
+        ],
+        totalFee: 2300,
+      },
+      parentId: "338710",
+      avgRatingString: "4.3",
+      totalRatingsString: "1K+",
+      sla: {
+        deliveryTime: 24,
+        lastMileTravel: 3.8,
+        serviceability: "SERVICEABLE",
+        slaString: "24 mins",
+        lastMileTravelString: "3.8 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2023-11-07 23:59:00",
+        opened: true,
+      },
+      badges: {},
+      isOpen: true,
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {},
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      aggregatedDiscountInfoV3: {
+        header: "₹175 OFF",
+        subHeader: "ABOVE ₹349",
+        discountTag: "FLAT DEAL",
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          mediaType: "ADS_MEDIA_ENUM_IMAGE",
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+    },
+    analytics: {},
+    cta: {
+      link: "https://www.swiggy.com/restaurants/-urban-egg-ghodod-road-athwa-surat-563098",
+      type: "WEBLINK",
+    },
+  },
+  {
+    info: {
+      id: "228880",
+      name: " Jalasai Locho & Khaman",
+      cloudinaryImageId: "w7alozosyqmbniqxyzao",
+      locality: "Gotalawadi",
+      areaName: "Katargam",
+      costForTwo: "₹200 for two",
+      cuisines: ["Gujarati", "North Indian", "Indian", "Sweets"],
+      avgRating: 4.2,
+      veg: true,
+      feeDetails: {
+        restaurantId: "228880",
+        fees: [
+          {
+            name: "BASE_DISTANCE",
+            fee: 4800,
+          },
+          {
+            name: "BASE_TIME",
+          },
+          {
+            name: "ANCILLARY_SURGE_FEE",
+          },
+        ],
+        totalFee: 4800,
+      },
+      parentId: "418110",
+      avgRatingString: "4.2",
+      totalRatingsString: "500+",
+      sla: {
+        deliveryTime: 22,
+        lastMileTravel: 6.8,
+        serviceability: "SERVICEABLE",
+        slaString: "22 mins",
+        lastMileTravelString: "6.8 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2023-11-07 19:45:00",
+        opened: true,
+      },
+      badges: {
+        imageBadges: [
+          {
+            imageId: "v1695133679/badges/Pure_Veg111.png",
+            description: "pureveg",
+          },
+        ],
+      },
+      isOpen: true,
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {
+            badgeObject: [
+              {
+                attributes: {
+                  description: "pureveg",
+                  imageId: "v1695133679/badges/Pure_Veg111.png",
+                },
+              },
+            ],
+          },
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      aggregatedDiscountInfoV3: {
+        header: "₹175 OFF",
+        subHeader: "ABOVE ₹349",
+        discountTag: "FLAT DEAL",
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          mediaType: "ADS_MEDIA_ENUM_IMAGE",
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+    },
+    analytics: {},
+    cta: {
+      link: "https://www.swiggy.com/restaurants/-jalasai-locho-and-khaman-gotalawadi-katargam-surat-228880",
+      type: "WEBLINK",
+    },
+  },
+];
+
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="search">Search</div>
+      <div className="res-container">
+        {resList.map((restaurant) => (
+          <RestaurantCard key={restaurant.info.id} resData={restaurant} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+const AppLayout = () => {
+  return (
+    <div className="app">
+      <Header />
+      <Body />
+    </div>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<AppLayout />);
